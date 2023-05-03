cc5f4c8aa51c2efc245243cebf02d11f3080adf5
Nate Klaiber <nate@theklaibers.com>
2023-05-03 15:32:41 -0400

    Adding corresponding requests

--- lib/stripe_platform/requests.rb
+++ lib/stripe_platform/requests.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../requests/models', __FILE__)
 require File.expand_path('../requests/directory', __FILE__)
 
+require File.expand_path('../requests/error_types', __FILE__)
+
+require File.expand_path('../requests/payout_statuses', __FILE__)
 require File.expand_path('../requests/payouts', __FILE__)
 
 module StripePlatform
