346d4cb76fe3b654c33c278e45731968c27b046b
Nate Klaiber <nate@theklaibers.com>
2023-05-03 15:32:54 -0400

    Payout request retrieve implementation

--- lib/stripe_platform/requests/payouts.rb
+++ lib/stripe_platform/requests/payouts.rb
@@ -26,6 +26,32 @@ module StripePlatform
 
         StripePlatform::Response.new(request)
       end
+
+      def self.retrieve(id, params={}, &block)
+        default_query_params = {}
+        default_query_params.merge!(StripePlatform::Client.default_query_params)
+
+        params = default_query_params.merge!(params)
+
+        route = StripePlatform::Client.routes.route_for('payout')
+        url   = route.url_for(params.merge!(id: id))
+
+        request = StripePlatform::Client.connection.get do |req|
+          if block_given?
+            if block.arity == 1
+              req.url(url)
+              yield(req)
+            else
+              req.url(url)
+              req.instance_eval(&block)
+            end
+          else
+            req.url(url)
+          end
+        end
+
+        StripePlatform::Response.new(request)
+      end
     end
   end
 end
