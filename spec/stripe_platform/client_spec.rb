43f8661fb0d19170e26b944141f9fb80228d358c
Nate Klaiber <nate@theklaibers.com>
2023-05-03 13:22:59 -0400

    initial commit

---
+++ spec/stripe_platform/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe StripePlatform::Client do
+  it "has a version number" do
+    expect(StripePlatform::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
