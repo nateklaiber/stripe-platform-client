96a8153f0454cf83a4151fc061e13d05e75ca541
Nate Klaiber <nate@theklaibers.com>
2023-05-03 13:55:50 -0400

    Adding a unit delegator (RubyUnit)
    This will be helpful for all currencies

---
+++ lib/stripe_platform/unit.rb
@@ -0,0 +1,28 @@
+require 'ruby_units/namespaced'
+
+module StripePlatform
+  class Unit < SimpleDelegator
+
+    # Returns an instance of the Unit library
+    #
+    # @param options
+    #
+    # @raise [StripePlatform::Errors::MissingUnitDefinitionError]
+    #
+    # @return [StripePlatform::Unit]
+    def initialize(*options)
+      begin
+        @_record = ::Unit.new(options)
+      rescue
+        raise StripePlatform::Errors::MissingUnitDefinitionError.new("Could not find unit definition for %s" % [*options])
+      end
+    end
+
+    # Implement simple delegator
+    #
+    # @return [noop]
+    def __getobj__
+      @_record
+    end
+  end
+end
