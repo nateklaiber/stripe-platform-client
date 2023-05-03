029bfc90c2186c7fcc0e5e79428f71685e5cc66b
Nate Klaiber <nate@theklaibers.com>
2023-05-03 15:33:17 -0400

    Adding the references to all models

--- lib/stripe_platform/models.rb
+++ lib/stripe_platform/models.rb
@@ -1,6 +1,17 @@
 require File.expand_path('../models/directory', __FILE__)
 require File.expand_path('../models/directory_entry', __FILE__)
 
+require File.expand_path('../models/error_types', __FILE__)
+require File.expand_path('../models/error_type', __FILE__)
+require File.expand_path('../models/error', __FILE__)
+
+require File.expand_path('../models/general_response', __FILE__)
+
+require File.expand_path('../models/payout_statuses', __FILE__)
+require File.expand_path('../models/payout_status', __FILE__)
+require File.expand_path('../models/payouts', __FILE__)
+require File.expand_path('../models/payout', __FILE__)
+
 module StripePlatform
   module Models
   end
